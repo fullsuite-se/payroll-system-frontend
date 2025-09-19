b80122372546f209ba413f75008ce7dd84f5effe
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-09-19 16:04:10 +0800

    WIP: add placeholder

---
+++ src/pages/main/configuration/company-configs/CompanyConfigsPage.jsx
@@ -0,0 +1,9 @@
+const CompanyConfigsPage = () => {
+    return (
+        <div>
+            this is CompanyConfigsPage,i.e., workdays, frequency etc
+        </div>
+    )
+};
+
+export default CompanyConfigsPage;
---
+++ src/pages/main/configuration/payitem/PayitemPage.jsx
@@ -0,0 +1,9 @@
+const PayitemPage = () => {
+    return (
+        <div>
+            this is payitem page
+        </div>
+    )
+};
+
+export default PayitemPage;
