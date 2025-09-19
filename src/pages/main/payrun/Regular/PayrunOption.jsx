7ad943cb8ebd1fbf24149b8ed6c879350bb8e72c
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-09-19 11:16:22 +0800

    chore: improve payrun option flow
    convert to form

--- src/pages/main/payrun/Regular/PayrunOption.jsx
+++ src/pages/main/payrun/Regular/PayrunOption.jsx
@@ -7,10 +7,18 @@ const PayrunOption = () => {
     const { options, handleInputChange, handlePayitemChange, removePayitem, handleGenerate, isValidating } = useRegularPayrunContext();
 
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        handleGenerate();
+    }
+
     return (
         <div className="bg-white p-6 rounded-xl border border-gray-200">
             {/* Main form grid */}
-            <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 mb-6">
+            <form
+                onSubmit={handleSubmit}
+                className="grid grid-cols-1 lg:grid-cols-5 gap-4 mb-6"
+            >
                 {/* Date From */}
                 <div className="space-y-2">
                     <label className="block text-xs font-medium text-gray-700">
@@ -86,47 +94,58 @@ const PayrunOption = () => {
                         <ChevronDownIcon className="absolute right-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400 pointer-events-none" />
                     </div>
                 </div>
-            </div>
+
+
+                {/* Draft/Generate  */}
+                <div className="space-y-2">
+                    <label className="block text-xs font-medium text-gray-700">
+                        Generate
+                    </label>
+                    <button
+                        type="submit"
+                        // className="w-full px-3 py-2.5  bg-teal-600 hover:bg-teal-700 text-white font-medium text-sm rounded-3xl "
+                        className="inline-flex items-center px-8 py-2.5 bg-teal-600 hover:bg-teal-700 text-white font-medium text-sm rounded-3xl "
+                    >
+                        {isValidating ? "Loading ..." : "Generate"}
+                    </button>
+
+
+                </div>
+            </form>
 
             {/* Selected Payitems (if any) */}
-            {options.pay_items.length > 0 && (
-                <div className="mb-6">
-                    <p className="text-xs font-medium text-gray-600 mb-2">Selected Pay Items:</p>
-                    <div className="flex flex-wrap gap-2">
-                        {options.pay_items.map((item, idx) => {
-                            const payitemId = Object.keys(item)[0];
-                            const payitemName = item[payitemId];
-                            return (
-                                <div
-                                    key={idx}
-                                    className="inline-flex items-center px-3 py-1.5 rounded-full text-xs text-teal-600 border border-blue-200"
-                                >
-                                    <span>{payitemName}</span>
-                                    <button
-                                        onClick={() => removePayitem(payitemId)}
-                                        className="ml-2 text-teal-500 hover:text-teal-700 focus:outline-none text-lg leading-none"
-                                        type="button"
-                                        aria-label={`Remove ${payitemName}`}
+            {
+                options.pay_items.length > 0 && (
+                    <div className="mb-6">
+                        <p className="text-xs font-medium text-gray-600 mb-2">Selected Pay Items:</p>
+                        <div className="flex flex-wrap gap-2">
+                            {options.pay_items.map((item, idx) => {
+                                const payitemId = Object.keys(item)[0];
+                                const payitemName = item[payitemId];
+                                return (
+                                    <div
+                                        key={idx}
+                                        className="inline-flex items-center px-3 py-1.5 rounded-full text-xs text-teal-600 border border-blue-200"
                                     >
-                                        ×
-                                    </button>
-                                </div>
-                            );
-                        })}
+                                        <span>{payitemName}</span>
+                                        <button
+                                            onClick={() => removePayitem(payitemId)}
+                                            className="ml-2 text-teal-500 hover:text-teal-700 focus:outline-none text-lg leading-none"
+                                            type="button"
+                                            aria-label={`Remove ${payitemName}`}
+                                        >
+                                            ×
+                                        </button>
+                                    </div>
+                                );
+                            })}
+                        </div>
                     </div>
-                </div>
-            )}
+                )
+            }
 
-            {/* Draft Button */}
-            <div className="flex justify-end">
-                <button
-                    onClick={handleGenerate}
-                    className="inline-flex items-center px-8 py-2.5 bg-teal-600 hover:bg-teal-700 text-white font-medium text-sm rounded-3xl "
-                >
-                    {isValidating ? "Loading ..." : "Generate"}
-                </button>
-            </div>
-        </div>
+
+        </div >
     );
 };
 
